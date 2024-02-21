dc10479ed4e441cd0e736644f79757d36fab216f
MorveN11 <morales.patty.jose@gmail.com>
2024-02-21 14:48:29 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# System programming
+
+## Members
+
+- Jose Manuel Morales Patty
+- Jheremy Kevin Tancara Zambrano
+- Santiago Quiroga Salazar
+- Andre Perez Duran
+- Rodrigo Esprella Valdez
+
---
+++ fijatemp.asm
@@ -0,0 +1,31 @@
+; asciiz  db  '12345', 0
+; temp    db  '123'
+; fija    db  5, '12345'
+; estruc  db  4, 3, '123'
+
+.model small
+.stack
+.data
+  cad1 db '100'
+  cad2 db 100 dup('?')
+
+.code
+  start:
+        lea si, cad1
+        lea bx, cad2
+        inc bx
+        mov cl, 3
+        xor dx, dx
+  ciclo:
+        mov al,[si]
+        mov [bx],al
+        inc si
+        inc bx
+        inc dx
+        dec cl
+        jnz ciclo
+
+        lea bx, cad2
+        mov [bx], dl
+  end start
+
